e9ca988b0e410cfe384c9977718a1e28bb509c7e
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-09-09 10:57:27 +0630

    here

--- seed.js
+++ seed.js
@@ -2,11 +2,33 @@ const User = require("./app/models/user");
 const mongoose = require("mongoose");
 const config = require("./config/db"); //get your mongoose string
 //create your array
-const createUser = User({
-    givenName:"RootUser",
-    email: "rootuser@gmail.com",
-    address:"Yangon",
-    password: "Root123"
+const createUser = User(
+  {
+    "givenName": "Kyaw Zaw Lwin",
+    "email": "rootuser@gmail.com",
+    "address": "",
+    "password": "Root123",
+    "DOB": null,
+    "gender": "Male",
+    "emergencyContact": "",
+    "phone": "09123192131",
+    "NRC": "",
+    "passportNo": "",
+    "educationBackground": "",
+    "other": [],
+    "recommendationLetter": [],
+    "firstInterviewDate": null,
+    "firstInterviewResult": "",
+    "secondInterviewDate": null,
+    "secondInterviewResult": "",
+    "employedDate": null,
+    "fatherName": "",
+    "isDeleted": false,
+    "casualLeaves": 6,
+    "medicalLeaves": 5,
+    "vacationLeaves": 5,
+    "maternityLeaveMale": 0,
+    "maternityLeaveFemale": 30,
   })
 
 //connect mongoose
@@ -23,10 +45,10 @@ mongoose
 //save your data. this is an async operation
 //after you make sure you seeded all the products, disconnect automatically
 
-createUser.save((err,result)=> {
-      console.log(result);
-      err && console.log(err)
-      console.log("admin create DONE!");
-      mongoose.disconnect();
-    
-  })
+createUser.save((err, result) => {
+  console.log(result);
+  err && console.log(err)
+  console.log("admin create DONE!");
+  mongoose.disconnect();
+
+})
