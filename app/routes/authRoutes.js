3ebfc41b735931d984511725c8d3825ee090bd23
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-09-11 16:54:46 +0630

    here

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -3,11 +3,12 @@
 const auth = require('../controllers/authController');
 const verifyToken = require('../lib/verifyToken');
 const { catchError } = require('../lib/errorHandler');
-
+const { verify } = require('crypto');
 
 module.exports = app => {
 
        app.route('/api/auth/login').post(auth.login);
 
        app.route('/api/auth/logout').get(verifyToken, catchError(auth.logout));
+       app.route('/api/auth/verify').get(auth.verifyToken)
 };
